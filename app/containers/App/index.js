b944139562bcf0f9dd89fea88d00fc1b772e75cf
magnussig <maggisig92@gmail.com>
2019-01-05 13:52:45 +0000

    add route param

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -39,7 +39,7 @@ export default function App() {
       </Helmet>
       <Header />
       <Switch>
-        <Route exact path="/" component={HomePage} />
+        <Route path="/:teamId?" component={HomePage} />
         <Route path="/features" component={FeaturePage} />
         <Route path="" component={NotFoundPage} />
       </Switch>
---
+++ app/containers/TeamListItem/index.js
@@ -0,0 +1,51 @@
+/**
+ * RepoListItem
+ *
+ * Lists the name and the issue count of a repository
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { FormattedNumber } from 'react-intl';
+
+import { makeSelectCurrentUser } from 'containers/App/selectors';
+import ListItem from 'components/ListItem';
+import IssueIcon from './IssueIcon';
+import IssueLink from './IssueLink';
+import RepoLink from './RepoLink';
+import Wrapper from './Wrapper';
+
+export class TeamListItem extends React.PureComponent {
+  render() {
+    const { item, match } = this.props;
+
+    // Put together the content of the repository
+    const content = (
+      <Wrapper>
+        <RepoLink href="https://www.theverge.com/" target="_blank">
+          {`Team name: ${item.name}`}
+        </RepoLink>
+        <IssueLink href="https://www.theverge.com/" target="_blank">
+          <IssueIcon />
+          <FormattedNumber value={match ? match.params.teamId : 69} />
+        </IssueLink>
+      </Wrapper>
+    );
+
+    // Render the content into a list item
+    return <ListItem key={`repo-list-item-${item.full_name}`} item={content} />;
+  }
+}
+
+TeamListItem.propTypes = {
+  item: PropTypes.object,
+  match: PropTypes.object,
+};
+
+export default connect(
+  createStructuredSelector({
+    currentUser: makeSelectCurrentUser(),
+  }),
+)(TeamListItem);
